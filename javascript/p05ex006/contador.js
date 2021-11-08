f993d89ad20ec3a78f9ba938d5f6011b52aebbfe
guanabara-eteot <91096819+guanabara-eteot@users.noreply.github.com>
2021-11-08 20:49:19 -0300

    Update contador.js

--- javascript/p05ex006/contador.js
+++ javascript/p05ex006/contador.js
@@ -1,11 +1,31 @@
 let saida = document.getElementById("saida")
+let msg = document.getElementById("mensagem")
 
 function contagem() {
     let ini = Number(document.getElementById("ini").value)
     let fim = Number(document.getElementById("fim").value)
     let passo = Number(document.getElementById("passo").value)
 
-    
+    if (passo == 0) { passo = 1 }
+    if (passo < 0) { passo *= -1 }
+
+    msg.innerHTML = `Contagem de ${ini} até ${fim} com salto ${passo}:<br><br>`
+
+    if (ini < fim) {
+        let cont = ini
+        while (cont <= fim) {
+            msg.innerHTML += `${cont} &#x1F449; `
+            cont ++ 
+        }
+    } else if (ini > fim) {
+        let cont = ini
+        while (cont >= fim) {
+            msg.innerHTML += `${cont} &#x1F449; `
+            cont -- 
+        }
+    }
+    msg.innerHTML += `&#x1F3C1; `
+
 
     return false
 }
