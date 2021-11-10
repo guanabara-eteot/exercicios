19484b67a25682561634ad9d09b9d82f1ef33afe
guanabara-eteot <91096819+guanabara-eteot@users.noreply.github.com>
2021-11-10 20:23:40 -0300

    Update script.js

--- javascript/parte05/p05ex017/script.js
+++ javascript/parte05/p05ex017/script.js
@@ -5,6 +5,7 @@ function primos() {
     let primo = true
     let cont = 0
     while (num <= 1000) {
+        // Esse aqui é um dos métodos mais eficientes para testar se um número é primo. Descartamos os valores 1 e o próprio número e testamos a partir do 2. Ao encontrar o primeiro divisível, o número não é primo e interrompemos a repetição com o comando break para não perder mais tempo descobrindo se existem outros divisores.
         primo = true
         cont = 2
         while (cont < num) {
